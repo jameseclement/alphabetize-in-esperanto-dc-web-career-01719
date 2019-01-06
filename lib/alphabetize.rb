e917a023e52c0cff7dd19980228aa9574d7ca3de
jameseclement <jameseclement@gmail.com>
2019-01-06 17:36:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,11 @@ esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 arr.sort_by {|phrase|
 
 phrase.split('').map {|letter|
+<<<<<<< HEAD
 
+=======
+binding.pry
+>>>>>>> 7ef2ae1c0d66f333b8d0b2e1528b5b4e7099b844
 esperanto_alphabet.index(letter)
 
 }
